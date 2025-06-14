2c047abdf48166bbfe5800c8dde9ad1deee8371d
Anudeep Debbata <30561588+Anudepp@users.noreply.github.com>
2025-06-14 10:43:00 +0530

    React elememt creation

--- App.js
+++ App.js
@@ -1,36 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1", key: "child1" }, [
-    // Added key
-    React.createElement(
-      "div",
-      { id: "grandchild1_1", key: "grandchild1_1" },
-      "Grandchild 1"
-    ), // Added unique key
-    React.createElement(
-      "div",
-      { id: "grandchild2_1", key: "grandchild2_1" },
-      "Grandchild 2"
-    ) // Added unique key
-  ]),
-  React.createElement("div", { id: "child2", key: "child2" }, [
-    // Added key
-    React.createElement(
-      "div",
-      { id: "grandchild1_2", key: "grandchild1_2" },
-      "Grandchild 1"
-    ), // Added unique key
-    React.createElement(
-      "div",
-      { id: "grandchild2_2", key: "grandchild2_2" },
-      "Grandchild 2"
-    ) // Added unique key
-  ])
-]);
-
+const heading = React.createElement("h1", { id: "heading" }, "Hello World");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
-
-console.log(parent);
+root.render(heading);
