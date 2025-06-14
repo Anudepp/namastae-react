3f60223988d43af1097b3faf3cc0cdd6ea2e1d69
Anudeep Debbata <30561588+Anudepp@users.noreply.github.com>
2025-06-14 19:00:51 +0530

    component composition

--- App.js
+++ App.js
@@ -1,6 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", { id: "heading" }, "Hello World");
+// jsx syntax for React elements
+const jsxHeading = <h1 id="heading">Hello World from JSX</h1>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(jsxHeading);
+console.log(jsxHeading);
+
+// React comnponent
+// functional component => new way of creating components
+
+// component name should start with capital letter, functional component is a function that returns JSX
+const Heading = () =>
+  <h1 id="heading">Hello World from Functional Component</h1>;
+
+const Heading2 = () =>
+  <div>
+    <h1 id="heading2" className="heading2">
+      Hello World from Functional Component 2
+    </h1>
+    <Heading />
+  </div>;
+
+// render the component
+root.render(<Heading2 />);
+
+// component composition, putting components inside other components.
