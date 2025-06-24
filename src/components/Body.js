4daac08d2c063df3e3c423a1274424c37c92db8b
Anudeep Debbata <30561588+Anudepp@users.noreply.github.com>
2025-06-24 16:53:35 +0530

    Used Swiggy Api to show list of restaurent

--- src/components/Body.js
+++ src/components/Body.js
@@ -8,7 +8,7 @@ const Body = () => {
   //the default value of the state variable is resList
   //resList is an array of restaurant objects imported from mockData.js
   //We can use the setListOfRestaurants function to update the state variable
-  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
 
   // Create a local state variable for search text
   // This will hold the text entered by the user in the search input field
@@ -18,8 +18,20 @@ const Body = () => {
 
   // Create a useEffect Hook
   useEffect(() => {
-    console.log("useEffect called");
+    fetchData();
   }, []);
+  // The empty dependency array means this effect will run only once when the component mounts
+  // Inside the useEffect, we call the fetchData function to fetch the restaurant data from the API and update the listOfRestaurants state variable with the fetched data
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.0072341&lng=79.55839209999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+    console.log(json);
+    setListOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants || []);
+  };
 
   return (
     <div className="body">
