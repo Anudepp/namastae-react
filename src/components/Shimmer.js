822836eb152cbb5331ac9e52f000377eda7db5de
Anudeep Debbata <30561588+Anudepp@users.noreply.github.com>
2025-06-25 11:48:57 +0530

    shimmer enhancement

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,14 +1,14 @@
 export const Shimmer = () => {
   return (
     <div className="shimmer-container">
-      <div className="shimmer-item"> cards</div>
-      <div className="shimmer-item"> cards</div>
-      <div className="shimmer-item"> cards</div>
-      <div className="shimmer-item"> cards</div>
-      <div className="shimmer-item"> cards</div>
-      <div className="shimmer-item"> cards</div>
-      <div className="shimmer-item"> cards</div>
-      <div className="shimmer-item"> cards</div>
+      <div className="shimmer-item"> </div>
+      <div className="shimmer-item"> </div>
+      <div className="shimmer-item"> </div>
+      <div className="shimmer-item"> </div>
+      <div className="shimmer-item"> </div>
+      <div className="shimmer-item"> </div>
+      <div className="shimmer-item"> </div>
+      <div className="shimmer-item"> </div>
     </div>
   );
 };
